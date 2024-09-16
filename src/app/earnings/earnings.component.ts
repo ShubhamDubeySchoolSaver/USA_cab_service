bb97d51404a7795cfbaa3f4596eb35afcaec47c8
Cundanreddy <cundan1209@gmail.com>
2024-09-17 00:55:28 +0530

    updated profile components and routed the components from navbar

--- src/app/earnings/earnings.component.ts
+++ src/app/earnings/earnings.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterLinkWithHref, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-earnings',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive,RouterLinkWithHref],
   templateUrl: './earnings.component.html',
   styleUrl: './earnings.component.css'
 })
 export class EarningsComponent {
   city="chennai"
-  fare= 300
+  fare= 300;
+
+  constructor(private router: Router){}
+
+  navigateToHome(){
+    this.router.navigate(['/']);
+  }
 }
 
