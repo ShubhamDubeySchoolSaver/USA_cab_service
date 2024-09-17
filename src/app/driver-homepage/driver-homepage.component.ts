d8a8fd15fb7e4e4f04409347b3465cc956c0c6dd
Cundanreddy <cundan1209@gmail.com>
2024-09-17 17:30:08 +0530

    updated routing and navbar

--- src/app/driver-homepage/driver-homepage.component.ts
+++ src/app/driver-homepage/driver-homepage.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RideRequestsComponent } from "../ride-requests/ride-requests.component";
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-driver-homepage',
   standalone: true,
-  imports: [],
+  imports: [RideRequestsComponent,RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './driver-homepage.component.html',
   styleUrl: './driver-homepage.component.css'
 })
 export class DriverHomepageComponent {
-
+  showRideRequest=true;
 }
