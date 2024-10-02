1443edc08106a08e33d8fb27b1c33fb65adbd0a2
Siva0443 <139725832+Siva0443@users.noreply.github.com>
2024-10-02 12:35:55 +0530

    driver-profile -update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NavbarComponent } from './driver_homepage/navbar/navbar.component';
 import { DriverHomepageComponent } from './driver_homepage/driver-homepage/driver-homepage.component';
 import { EarningsComponent } from './driver_homepage/earnings/earnings.component';
 import { RideRequestsComponent } from './driver_homepage/ride-requests/ride-requests.component';
+import { DriverProfileUpdateComponent } from './driver-profile-update/driver-profile-update.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,EarningsComponent,RideRequestsComponent,DriverHomepageComponent,FoDelCusHomPComponent],
+  imports: [RouterOutlet, NavbarComponent,EarningsComponent,RideRequestsComponent,DriverHomepageComponent,FoDelCusHomPComponent,DriverProfileUpdateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
